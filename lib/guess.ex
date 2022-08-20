afec943ec9f7fe4aba04b68300d62c290ba12a84
Blacksmith <davenstall@gmail.com>
2022-08-19 21:40:22 -0300

    Primeira refatoração

--- lib/guess.ex
+++ lib/guess.ex
@@ -10,17 +10,44 @@ defmodule Guess do
     IO.puts("Let's play Guess the Number")
 
     IO.gets("Pick a difficult level (1, 2 or 3):")
-    |> Integer.parse()
     |> parse_input()
+    |> pickup_number()
     |> IO.inspect()
   end
 
+  def pickup_number(level) do
+    level
+    |> get_range()
+    |> Enum.random()
+  end
+
   def parse_input(:error) do
-    IO.puts("Invalid level")
+    IO.puts("Invalid input")
     run()
   end
 
-  def parse_input({num, _}) do
-    num
+  def parse_input({num, _}), do: num
+
+  def parse_input(data) do
+    data
+    |> Integer.parse()
+    |> parse_input()
+  end
+
+  def get_range(level) do
+    case level do
+      1 ->
+        1..10
+
+      2 ->
+        1..100
+
+      3 ->
+        1..1000
+
+      _ ->
+        IO.puts("Invalid level!")
+        run()
+    end
   end
 end
